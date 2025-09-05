72aca6362b380ead0fea69f6c3cbaba99836967b
reljajocic <149072501+reljajocic@users.noreply.github.com>
2025-09-05 12:47:08 +0200

    fix: location cache

--- Backend/jpa-example/src/main/java/rs/ac/uns/ftn/informatika/jpa/config/CacheConfig.java
+++ Backend/jpa-example/src/main/java/rs/ac/uns/ftn/informatika/jpa/config/CacheConfig.java
@@ -5,13 +5,16 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 @EnableCaching
 public class CacheConfig {
 
     @Bean
+    @Primary
     public CacheManager cacheManager() {
-        return new ConcurrentMapCacheManager("topPostsAllTime", "topPostsLastWeek");
+        return new ConcurrentMapCacheManager("locations", "topPostsAllTime", "topPostsLastWeek");
+        // pozdrav za kolegu Miloslava koji je ovo dodao i nije mi rekao i onda mi nije radilo kreiranje lokacija
     }
 }
